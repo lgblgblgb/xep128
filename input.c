627cfde886ef00e90fb9ad8f6a4c9d0a3730c8ee
LGB <lgblgblgb@gmail.com>
2015-05-18 12:53:27 +0200

    License/copyright comment in files. Very important :-P

--- input.c
+++ input.c
@@ -1,5 +1,21 @@
-/* Human Input Devices emulation,
- * ie keyboard, mouse, joystick */
+/* Xep128: Minimalistic Enterprise-128 emulator with focus on "exotic" hardware
+   Copyright (C)2015 LGB (Gábor Lénárt) <lgblgblgb@gmail.com>
+   http://xep128.lgb.hu/
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
 
 #include "xepem.h"
 
