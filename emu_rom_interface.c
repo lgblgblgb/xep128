1cdd4d46d3d9231d6b9b8b2eb6972011345aef18
LGB <lgblgblgb@gmail.com>
2015-06-10 16:35:09 +0200

    SDL pref/base path can be NULL.

--- emu_rom_interface.c
+++ emu_rom_interface.c
@@ -119,7 +119,8 @@ static void cmd_emu ( void )
 		getenv("USERNAME"), buf, OS_KIND, SDL_GetPlatform(),
 		sdlver_c.major, sdlver_c.minor, sdlver_c.patch,
 		sdlver_l.major, sdlver_l.minor, sdlver_l.patch,
-		app_base_path, app_pref_path
+		app_base_path ? app_base_path : "<ERROR>",
+		app_pref_path ? app_pref_path : "<ERROR>"
 	);
 }
 
