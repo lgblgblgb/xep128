9d64cb8b3e0bf102d58f57b436e0d4a04b3684cb
LGB <lgblgblgb@gmail.com>
2016-04-02 02:35:36 +0200

    Joystick hat support as virtual axes

--- joystick.c
+++ joystick.c
@@ -7,43 +7,45 @@
 
 static struct {
 	SDL_Joystick *joy;
-	int sdl_index;
+	int	sdl_index;
 	int	num_of_buttons;
 	int	num_of_axes;
+	int	num_of_axes_orig;	// before abstraction of hats as axes ...
+	int	num_of_hats;
 	Uint32	button;
 	Uint32	axisp;
 	Uint32	axisn;
 	const char *name;
 	SDL_JoystickGUID guid;
-	char guid_str[40];
-	Uint32 button_mask;
-	Uint32 axis_mask;
+	char	guid_str[40];
 } joysticks[MAX_JOYSTICKS];
 static struct {
 	int	id;	// Joystick ID (in joysticks array), use -1 for numeric keypad, or -2 for disable!, for vals < 0, all other options here ARE NOT used at all!
 	Uint32	button_masks[3];
 	Uint32	v_axis_mask, h_axis_mask;
-} epexjoys[3];	// emulation details for the two external joysticks of Enterprise (note: we have 0,1 on EP they're named as 1,2!) index 2 is for internal joystick emu ...
+} epjoys[3];	// emulation details for the two external joysticks of Enterprise (note: we have 0,1 on EP they're named as 1,2!) index 2 is for internal joystick emu ...
 static int joy_to_init = 1;
 static const char unknown_joystick_name[] = "Unknown joystick";
 
 
-
 #define BIT_TO_SET(storage,mask) (storage) |= mask
 #define BIT_TO_RESET(storage,mask) (storage) &= (4294967295U - mask)
-
 
 #define AXIS_LIMIT_HIGH	20000
 #define AXIS_LIMIT_LOW	10000
 
 
 
-
-static void set_axis_state ( int joy_id, Uint32 axis, int value )
+/* virtual is zero for "real" axes,
+   for hat->axis abstraction, axes will mean the hat number, and virtual must be 1 or 2 for the two
+   "axes" of the hat */
+static void set_axis_state ( int joy_id, Uint32 axis, int value, int virtual )
 {
 	Uint32 axis_mask, oldp, oldn;
-	if (joy_id >= MAX_JOYSTICKS || !joysticks[joy_id].joy || axis >= joysticks[joy_id].num_of_axes)
+	if (joy_id >= MAX_JOYSTICKS || !joysticks[joy_id].joy || axis >= (virtual ? joysticks[joy_id].num_of_axes : joysticks[joy_id].num_of_axes_orig))
 		return;
+	if (virtual)
+		axis = joysticks[joy_id].num_of_axes_orig + axis * 2 + virtual - 1;
 	axis_mask = 1U << axis;
 	oldp = joysticks[joy_id].axisp; 
 	oldn = joysticks[joy_id].axisn;
@@ -58,13 +60,14 @@ static void set_axis_state ( int joy_id, Uint32 axis, int value )
 		BIT_TO_SET  (joysticks[joy_id].axisn, axis_mask);
 	}
 	if (show_keys && (oldp != joysticks[joy_id].axisp || oldn != joysticks[joy_id].axisn))
-		OSD("PC joystick #%d axis %d [%c %c]\nmask=%X value=%d",
+		OSD("PC joystick #%d axis %d [%c %c]\nmask=%X value=%d virtual=%c",
 			joy_id,
 			axis,
 			(joysticks[joy_id].axisn & axis_mask) ? '-' : ' ',
 			(joysticks[joy_id].axisp & axis_mask) ? '+' : ' ',
 			axis_mask,
-			value
+			value,
+			virtual ? 'Y' : 'N'
 	);
 }
 
@@ -82,7 +85,28 @@ static void set_button_state ( int joy_id, Uint32 button, int value )
 	else
 		BIT_TO_RESET(joysticks[joy_id].button, button_mask);
 	if (show_keys && old != joysticks[joy_id].button)
-		OSD("PC joystick #%d button %d %s", joy_id, button, value ? "pressed" : "released");
+		OSD("PC joystick #%d button %d\n%s", joy_id, button, value ? "pressed" : "released");
+}
+
+
+
+static void set_hat_state ( int joy_id, Uint32 hat, int value )
+{
+	if (joy_id >= MAX_JOYSTICKS || !joysticks[joy_id].joy || hat >= joysticks[joy_id].num_of_hats)
+		return;
+	// we emulate hats as two axes after placed the "normal axes"
+	if (value & SDL_HAT_UP)
+		set_axis_state(joy_id, hat, -32000, 1);
+	else if (value & SDL_HAT_DOWN)
+		set_axis_state(joy_id, hat,  32000, 1);
+	else
+		set_axis_state(joy_id, hat,      0, 1);
+	if (value & SDL_HAT_LEFT)
+		set_axis_state(joy_id, hat, -32000, 2);
+	else if (value & SDL_HAT_RIGHT)
+		set_axis_state(joy_id, hat,  32000, 2);
+	else
+		set_axis_state(joy_id, hat,      0, 2);
 }
 
 
@@ -94,10 +118,12 @@ static void joy_sync ( int joy_id )
 		return;
 	joysticks[joy_id].button = joysticks[joy_id].axisp = joysticks[joy_id].axisn = 0;
 	for (a = 0; a < 32; a++) {
-		if (a < joysticks[joy_id].num_of_axes)
-			set_axis_state(joy_id, a, SDL_JoystickGetAxis(joysticks[joy_id].joy, a));
+		if (a < joysticks[joy_id].num_of_axes_orig)
+			set_axis_state(joy_id, a, SDL_JoystickGetAxis(joysticks[joy_id].joy, a), 0);
 		if (a < joysticks[joy_id].num_of_buttons)
 			set_button_state(joy_id, a, SDL_JoystickGetButton(joysticks[joy_id].joy, a));
+		if (a < joysticks[joy_id].num_of_hats)
+			set_hat_state(joy_id, a, SDL_JoystickGetHat(joysticks[joy_id].joy, a));
 	}
 }
 
@@ -130,7 +156,8 @@ static void joy_attach ( int joy_id )
 		return;
 	joysticks[joy_id].sdl_index = joy_id;
 	joysticks[joy_id].num_of_buttons = SDL_JoystickNumButtons(joy);
-	joysticks[joy_id].num_of_axes =  SDL_JoystickNumAxes(joy);
+	joysticks[joy_id].num_of_axes = SDL_JoystickNumAxes(joy);
+	joysticks[joy_id].num_of_hats = SDL_JoystickNumHats(joy);
 	joysticks[joy_id].name = SDL_JoystickName(joy);
 	if (!joysticks[joy_id].name)
 		joysticks[joy_id].name = unknown_joystick_name;
@@ -144,17 +171,28 @@ static void joy_attach ( int joy_id )
 		joysticks[joy_id].num_of_buttons = 32;
 	if (joysticks[joy_id].num_of_axes > 32)
 		joysticks[joy_id].num_of_axes = 32;
+	if (joysticks[joy_id].num_of_hats > 32)
+		joysticks[joy_id].num_of_hats = 32;
+	if (joysticks[joy_id].num_of_axes + (joysticks[joy_id].num_of_hats * 2) > 32) {
+		if (joysticks[joy_id].num_of_hats > 4)
+			joysticks[joy_id].num_of_hats = 4;
+	}
+	if (joysticks[joy_id].num_of_axes + (joysticks[joy_id].num_of_hats * 2) > 32) {
+		joysticks[joy_id].num_of_axes = 32 - (joysticks[joy_id].num_of_hats * 2);
+	}
 	joy_sync(joy_id);
-	DEBUG("JOY: new device added #%d \"%s\" axes=%d buttons=%d balls=%d hats=%d guid=%s" NL,
+	DEBUGPRINT("JOY: new device added #%d \"%s\" axes=%d buttons=%d (balls=%d) hats=%d guid=%s" NL,
 		joy_id,
 		joysticks[joy_id].name,
 		joysticks[joy_id].num_of_axes,
 		joysticks[joy_id].num_of_buttons,
 		SDL_JoystickNumBalls(joy),
-		SDL_JoystickNumHats(joy),
+		joysticks[joy_id].num_of_hats,
 		joysticks[joy_id].guid_str
 	);
 	OSD("Joy attached:\n#%d %s", joy_id, joysticks[joy_id].name);
+	joysticks[joy_id].num_of_axes_orig = joysticks[joy_id].num_of_axes;
+	joysticks[joy_id].num_of_axes += joysticks[joy_id].num_of_hats * 2;
 }
 
 
@@ -168,12 +206,12 @@ void joy_sdl_event ( SDL_Event *e )
 			joysticks[a].button = joysticks[a].axisp = joysticks[a].axisn = 0;
 		}
 		for (a = 0; a < 2; a++) {
-			epexjoys[a].id = 0;	// assigned to SDL joystick #0
-			epexjoys[a].button_masks[0] = 1;
-			epexjoys[a].button_masks[1] = 0;
-			epexjoys[a].button_masks[2] = 0;
-			epexjoys[a].v_axis_mask = 2;
-			epexjoys[a].h_axis_mask = 1;
+			epjoys[a].id = 0;	// assigned to SDL joystick #0
+			epjoys[a].button_masks[0] = 1;
+			epjoys[a].button_masks[1] = 0;
+			epjoys[a].button_masks[2] = 0;
+			epjoys[a].v_axis_mask = 2;
+			epjoys[a].h_axis_mask = 1;
 		}
 		SDL_GameControllerEventState(SDL_DISABLE);
 		SDL_JoystickEventState(SDL_ENABLE);
@@ -181,7 +219,7 @@ void joy_sdl_event ( SDL_Event *e )
 	}
 	if (e) switch (e->type) {
 		case SDL_JOYAXISMOTION:	// joystick axis motion
-			set_axis_state(e->jaxis.which, e->jaxis.axis, e->jaxis.value);
+			set_axis_state(e->jaxis.which, e->jaxis.axis, e->jaxis.value, 0);
 			break;
 		case SDL_JOYBUTTONDOWN:
 		case SDL_JOYBUTTONUP:
@@ -192,6 +230,9 @@ void joy_sdl_event ( SDL_Event *e )
 			break;
 		case SDL_JOYDEVICEREMOVED:
 			joy_detach(e->jdevice.which);
+			break;
+		case SDL_JOYHATMOTION:
+			set_hat_state(e->jhat.which, e->jhat.hat, e->jhat.value);
 			break;
 	}
 }
@@ -209,7 +250,7 @@ int joystick_scan ( int num, int dir )
 {
 	if (num != 0 && num != 1)
 		return 0;
-	switch (epexjoys[num].id) {
+	switch (epjoys[num].id) {
 		case EPJOY_KP:
 			return !(kbd_matrix[10] & (1 << dir));  // keyboard-matrix row #10 (not a real EP one!) is used to maintain status of numeric keypad joy emu keys ...
 		case EPJOY_DISABLED:
@@ -220,19 +261,19 @@ int joystick_scan ( int num, int dir )
 				return 1;
 			switch (dir) {
 				case JOY_SCAN_FIRE1:
-					return joysticks[epexjoys[num].id].button & epexjoys[num].button_masks[0];
+					return joysticks[epjoys[num].id].button & epjoys[num].button_masks[0];
 				case JOY_SCAN_UP:
-					return joysticks[epexjoys[num].id].axisn  & epexjoys[num].v_axis_mask;
+					return joysticks[epjoys[num].id].axisn  & epjoys[num].v_axis_mask;
 				case JOY_SCAN_DOWN:
-					return joysticks[epexjoys[num].id].axisp  & epexjoys[num].v_axis_mask;
+					return joysticks[epjoys[num].id].axisp  & epjoys[num].v_axis_mask;
 				case JOY_SCAN_LEFT:
-					return joysticks[epexjoys[num].id].axisn  & epexjoys[num].h_axis_mask;
+					return joysticks[epjoys[num].id].axisn  & epjoys[num].h_axis_mask;
 				case JOY_SCAN_RIGHT:
-					return joysticks[epexjoys[num].id].axisp  & epexjoys[num].h_axis_mask;
+					return joysticks[epjoys[num].id].axisp  & epjoys[num].h_axis_mask;
 				case JOY_SCAN_FIRE2:
-					return joysticks[epexjoys[num].id].button & epexjoys[num].button_masks[1];
+					return joysticks[epjoys[num].id].button & epjoys[num].button_masks[1];
 				case JOY_SCAN_FIRE3:
-					return joysticks[epexjoys[num].id].button & epexjoys[num].button_masks[2];
+					return joysticks[epjoys[num].id].button & epjoys[num].button_masks[2];
 			}
 	}
 	return 0;	// unknown?!
