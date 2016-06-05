6192a46a6a0b0ea38f4cf674bbe8f46dc8be8730
LGB <lgblgblgb@gmail.com>
2016-06-05 02:52:41 +0200

    XCPM (new name from Zozo) bugfixes + README

--- tests/cpm-minimal-emulator.c
+++ tests/cpm-minimal-emulator.c
@@ -1,3 +1,27 @@
+/* This is a MINIMAL CP/M emulator. That is, it emulates a Z80 (though 8080 would be
+   enough for most CP/M software), the BDOS and the CBIOS as well, and just expects
+   a CP/M program to load and run. It was written *ONLY FOR* running some ancient
+   tools, namely M80 assembler and L80 linker (they don't seem to have modern versions
+   for non-CP/M OSes). The emulator has BDOS/CBIOS functions implemented and in a way
+   that is enough for these tools! Maybe this can change in the future, but this is the
+   current situation! Stderr should be redirected, as those are DEBUG messages.
+
+   Copyright (C)2016 LGB (Gábor Lénárt) <lgblgblgb@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -171,11 +195,12 @@ static void fcb_to_filename ( Uint16 fcb_addr, char *fn )
 			}
 			if (a > 9)
 				break;
-			*(fn++) = '.';
+			//*(fn++) = '.';
 			a = 8;	// this will be pre-incremented in the next iteration
 		} else {
+			if (a == 9)
+				*(fn++) = '.';
 			*(fn++) = (c >= 'a' && c <= 'z') ? c - 0x20 : c;
-			
 		}
 	}
 	*fn = 0;
@@ -243,7 +268,7 @@ static int bdos_open_file ( Uint16 fcb_addr, int is_create )
 			DEBUG("CPM: FATAL: FILE CREATE FUNC, but file %s existed before, stopping!\n", fn);
 			exit(1);
 		}
-		f = fopen(fn, "w+");
+		f = fopen(fn, "w+b");
 	} else
 		f = fopen(fn, "r+b");
 	if (!f) {
